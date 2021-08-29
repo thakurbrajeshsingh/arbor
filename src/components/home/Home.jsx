a1d67b6d81cab5d43e881c77f4fa8d2d1d8f52b1
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-30 01:35:50 +0530

    Img Source Added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+// Components
+import Navbar from "./Navbar";
 
-
-
-const Home =()=>{
-    return(
-        <>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
 export default Home;
---
+++ src/components/home/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { navData } from '../../constants/Data';
+
+
+
+
+const Navbar = () =>{
+    return(
+        <>
+       
+        </>
+    )
+}
+
+
+export default Navbar;
