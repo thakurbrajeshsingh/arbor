5fcb5ca265f8ce335286432b5f767833de38e94c
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-29 22:55:26 +0530

    Home Section Start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // components
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 
 const App = () => {
   return (
     <>
       <Header />
-      
+      <Home />
     </>
   )
 }
