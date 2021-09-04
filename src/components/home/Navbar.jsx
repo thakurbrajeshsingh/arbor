054a093cee6e1fb2d9d65b7d4878caf12b512e1c
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-09-05 01:21:13 +0530

    Images Text

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { navData } from '../../constants/Data';
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import { navData } from "../../constants/Data";
 
-
-
-
-const Navbar = () =>{
-    return(
-        <>
-       
-        </>
-    )
-}
-
+const Navbar = () => {
+  return (
+    <Box>
+      {navData.map((data) => (
+        <Box>
+          <img src={data.url} alt="" />
+          <Typography>{data.text}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
 
 export default Navbar;
