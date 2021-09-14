c1a4b46ffa74b66a2e178b4dd5700187a1ed2b31
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-09-14 23:27:46 +0530

    Connect Mongoose DB Cloud

--- server/index.js
+++ server/index.js
@@ -6,12 +6,19 @@ const PORT = 8000;
 
 const app = express();
 
-const URL='mongodb+srv://brajesh:Brajesh@963@arbor.7tj3x.mongodb.net/ARBOR?retryWrites=true&w=majority'
+const URL = 'mongodb://brajesh:Brajesh123@arborserver-shard-00-00.zzbrl.mongodb.net:27017,arborserver-shard-00-01.zzbrl.mongodb.net:27017,arborserver-shard-00-02.zzbrl.mongodb.net:27017/ARBOR?ssl=true&replicaSet=atlas-cj1o7z-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 
-mongoose.connection = () => {
-    mongoose.connect(URL, {useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
+const Connection = async () => {
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true})
+        console.log("Database Connected successfully");
+    } catch (error) {
+        console.log("Error in connecting DB", error.message);
+    }
 }
+
+Connection();
 
 app.listen(PORT, (err) => {
     if (err) {
