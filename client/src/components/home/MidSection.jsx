32dab45d24aeaac19d07dab098536f039392e191
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-09-18 02:02:53 +0530

    ShoutOut Section Image Added

--- client/src/components/home/MidSection.jsx
+++ client/src/components/home/MidSection.jsx
@@ -6,20 +6,29 @@ import { imageURL } from "../../constants/Data";
 const useStyle = makeStyles({
   wrapper: {
     display: "flex",
-    marginTop:20,
-    justifyContent:"space-between"
+    marginTop: 20,
+    justifyContent: "space-between",
   },
 });
 
 const Midsection = () => {
   const classes = useStyle();
+  const coronaURL =
+    "https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50";
+
   return (
-    <Box className={classes.wrapper}>
-      {imageURL.map(image => (
-        <img src={image} alt="Loading" style={{width:"33%"}} />
-      ))
-      }
-    </Box>
+    <>
+      <Box className={classes.wrapper}>
+        {imageURL.map((image) => (
+          <img src={image} alt="Loading" style={{ width: "33%" }} />
+        ))}
+      </Box>
+      <img
+        src={coronaURL}
+        alt="Loading..."
+        style={{ width: "100%", marginTop: "20px" }}
+      />
+    </>
   );
 };
 
