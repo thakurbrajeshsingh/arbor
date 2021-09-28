9c43bb901edae80c10f49636a14906b218ecc1df
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-09-29 00:32:40 +0530

    Login Css Added

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -10,12 +10,12 @@ import {
 
 const useStyles = makeStyles({
   component: {
-    height: "70vh",
-    width: "90vh",
+    height: "80vh",
+    width: "100vh",
   },
   image: {
     backgroundImage: `url(${"https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png"})`,
-    height: "70vh",
+    height: "80vh",
     backgroundRepeat: "no-repeat",
     background: "#2874f0",
     width: "40%",
@@ -25,6 +25,42 @@ const useStyles = makeStyles({
       color: "#FFFFFF",
       fontWeight: 600,
     },
+  },
+  login: {
+    padding: "25px 35px",
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    "& > *": {
+      marginTop: 20,
+    },
+  },
+  text: {
+    color: "#878787",
+    fontSize: 12,
+  },
+  loginBtn: {
+    textTransform: "none",
+    background: "#FB641B",
+    color: "#FFFFFF",
+    height: 48,
+    borderRadius: 2,
+  },
+  requestBtn: {
+    textTransform: "none",
+    background: "#FFFFFF",
+    color: "#2874f0",
+    height: 48,
+    borderRadius: 2,
+    boxShadow: "0 2px 4px 0 rgb(0 0 0 / 20% )",
+  },
+  createText: {
+    textAlign: "center",
+    marginTop: "auto",
+    fontSize: 14,
+    color: "#2874f0",
+    fontWeight: 600,
+    cursor: "pointer",
   },
 });
 
@@ -36,31 +72,41 @@ const Login = ({ open, setOpen }) => {
   };
 
   return (
-    <>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogContent className={classes.component}>
-          <Box style={{ display: "flex" }}>
-            <Box className={classes.image}>
-              <Typography variant="h5">Login</Typography>
-              <Typography style={{ marginTop: "20px" }}>
-                Get Access to your orders,wishlist and Recommendations
-              </Typography>
-            </Box>
-            <Box></Box>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogContent className={classes.component}>
+        <Box style={{ display: "flex" }}>
+          <Box className={classes.image}>
+            <Typography variant="h5">Login</Typography>
+            <Typography style={{ marginTop: "20px" }}>
+              Get Access to your orders,wishlist and Recommendations
+            </Typography>
           </Box>
-          <TextField />
-          <TextField />
-          <Typography>
-            By continuing, you agree to Flipkart's Terms of use of Privacy
-            Policy
-          </Typography>
-          <Button>Login</Button>
-          <Typography>OR</Typography>
-          <Typography>Request OTP</Typography>
-          <Typography>New to Flipkart? Create Account </Typography>
-        </DialogContent>
-      </Dialog>
-    </>
+          <Box className={classes.login}>
+            <TextField name="username" label="Enter Email/Mobile Number" />
+            <TextField name="password" label="Enter Password" />
+            <Typography className={classes.text}>
+              By continuing, you agree to Flipkart's Terms of use of Privacy
+              Policy
+            </Typography>
+            <Button variant="contained" className={classes.loginBtn}>
+              Login
+            </Button>
+            <Typography
+              style={{ textAlign: "center" }}
+              className={classes.text}
+            >
+              OR
+            </Typography>
+            <Button variant="contained" className={classes.requestBtn}>
+              Request OTP
+            </Button>
+            <Typography className={classes.createText}>
+              New to Flipkart? Create Account{" "}
+            </Typography>
+          </Box>
+        </Box>
+      </DialogContent>
+    </Dialog>
   );
 };
 
